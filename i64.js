9dfb8e8c7928d910342256b2898b513d4ce93bf0
Kris Kowal <kris@uber.com>
2015-09-26 18:49:44 -0700

    Support further i64 representations

--- i64.js
+++ i64.js
@@ -36,36 +36,74 @@ var I64RW = bufrw.AtomRW(8,
         if (value instanceof Buffer) {
             value.copy(buffer, offset, 0, 8);
             return new bufrw.WriteResult(null, offset + 8);
-
         } else if (typeof value === 'number') {
             buffer.writeInt32BE(0, offset, true);
             buffer.writeInt32BE(value, offset + 4, true);
             return new bufrw.WriteResult(null, offset + 8);
-
+        } else if (Array.isArray(value)) {
+            return writeArrayInt64Into(value, buffer, offset);
         } else if (typeof value === 'string') {
-            if (value.length !== 16) {
-                return bufrw.WriteResult.error(errors.expected(value, 'a string of 16 hex characters'));
-            }
-
-            var hi = parseInt(value.slice(0, 8), 16);
-            if (hi !== hi) { // NaN
-                return bufrw.WriteResult.error(errors.expected(value, 'a string of hex characters'));
-            }
-            var lo = parseInt(value.slice(8, 16), 16);
-            if (lo !== lo) { // NaN
-                return bufrw.WriteResult.error(errors.expected(value, 'a string of hex characters'));
-            }
-
-            buffer.writeInt32BE(hi, offset);
-            buffer.writeInt32BE(lo, offset + 4);
-            return new bufrw.WriteResult(null, offset + 8);
-
+            return writeStringInt64Into(value, buffer, offset);
+        } else if (value && typeof value === 'object') {
+            return writeObjectInt64Into(value, buffer, offset);
         } else {
-            return bufrw.WriteResult.error(errors.expected(value, 'a buffer, a small number, or a string of 16 hex digits'));
+            return bufrw.WriteResult.error(errors.expected(value,
+                'i64 representation'));
         }
     });
 
-// TODO decide whether to do buffer or [hi, lo] based on annotations
+function writeObjectInt64Into(value, buffer, offset) {
+    if (typeof value.hi !== 'number') {
+        return bufrw.WriteResult.error(errors.expected(value,
+            '{hi, lo} with hi number, or other i64 representation'));
+    }
+    if (typeof value.lo !== 'number') {
+        return bufrw.WriteResult.error(errors.expected(value,
+            '{hi, lo} with lo number, or other i64 representation'));
+    }
+    // Does not validate range of hi or lo value
+    buffer.writeInt32BE(value.hi, offset, true);
+    buffer.writeInt32BE(value.lo, offset + 4, true);
+    return new bufrw.WriteResult(null, offset + 8);
+}
+
+function writeArrayInt64Into(value, buffer, offset) {
+    if (value.length !== 8) {
+        return new bufrw.WriteResult(errors.expected(value,
+            'an array of 8 bytes, or other i64 representation'));
+    }
+    // Does not validate individual byte values, particularly allowing unsigned
+    // or signed byte values without discrimination.
+    for (var index = 0; index < 8; index++) {
+        buffer[offset + index] = value[index] & 0xFF;
+    }
+    return new bufrw.WriteResult(null, offset + 8);
+}
+
+function writeStringInt64Into(value, buffer, offset) {
+    if (value.length !== 16) {
+        return new bufrw.WriteResult(errors.expected(value,
+            'a string of 16 hex characters, or other i64 representation'));
+    }
+
+    var hi = parseInt(value.slice(0, 8), 16);
+    if (hi !== hi) { // NaN
+        return new bufrw.WriteResult(errors.expected(value,
+            'a string of hex characters, or other i64 representation'));
+    }
+    var lo = parseInt(value.slice(8, 16), 16);
+    if (lo !== lo) { // NaN
+        return new bufrw.WriteResult(errors.expected(value,
+            'a string of hex characters, or other i64 representation'));
+    }
+
+    buffer.writeInt32BE(hi, offset);
+    buffer.writeInt32BE(lo, offset + 4);
+    return new bufrw.WriteResult(null, offset + 8);
+}
+
+// TODO support for reading other supported i64 representations off buffers
+// with specified js.type annotation.
 function ThriftI64() { }
 
 ThriftI64.prototype.rw = I64RW;
